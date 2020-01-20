4540d3f5bf255e9be1f3b9964563ea9767d8180a
april <april.peace@autotrader.co.uk>
2020-01-20 12:12:50 +0000

    Diesel and petrol tests pass

--- exercises/java/src/main/java/tax/CalculateTax.java
+++ exercises/java/src/main/java/tax/CalculateTax.java
@@ -8,45 +8,61 @@ public class CalculateTax extends TaxCalculator {
 
 int[] petrol = {0, 10, 25, 105, 125, 145, 165, 205, 515, 830, 1240, 1760, 2070 };
     int[] diesel = {0, 25, 105, 125, 145, 165, 205, 515, 830, 1240, 1760, 2070, 2070 };
-
+int[] arrayToUse;
     public int calculateTax(Vehicle vehicle) {
-
+if(vehicle.getFuelType().equals(FuelType.PETROL)){arrayToUse = petrol;}
+else{arrayToUse = diesel;}
             if(vehicle.getCo2Emissions()>255){
-                return 2070;
+                return arrayToUse[12];
+                //return 2070;
             }
             if(vehicle.getCo2Emissions()>225)
-            {
-                return 1760;
+            {return arrayToUse[11];
+                //return 1760;
             }
             if(vehicle.getCo2Emissions()>190){
-                return 1240;
+                return arrayToUse[10];
+                //return 1240;
             }
             if(vehicle.getCo2Emissions()>170){
-                return 830;
+                return arrayToUse[9];
+                //return 830;
             }
             if(vehicle.getCo2Emissions()>150){
-                return 515;
+                return arrayToUse[8];
+               // return 515;
             }
             if(vehicle.getCo2Emissions()>130){
-                return 205;
+                return arrayToUse[7];
+                //return 205;
             }
             if(vehicle.getCo2Emissions()>110){
-                return 165;
+                return arrayToUse[6];
+                //return 165;
             }
         if(vehicle.getCo2Emissions()>100){
-            return 145;
+            return arrayToUse[5];
+            // return 145;
         }
         if(vehicle.getCo2Emissions()>90){
-            return 125;
+            return arrayToUse[4];
+
+            //  return 125;
         }
         if(vehicle.getCo2Emissions()>75){
-            return 105;
+            return arrayToUse[3];
+
+            //return 105;
         }
         if(vehicle.getCo2Emissions()>50){
-            return 25;
+            return arrayToUse[2];
+
+            //return 25;
         }
         if(vehicle.getCo2Emissions()>1){
-            return 10;
+            return arrayToUse[1];
+
+            // return 10;
         }
             return 0;
 
